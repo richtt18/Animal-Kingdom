be4321a9813ff91d23cbbb67f4e11588fcc43d5a
varan2030 <varan2030@mail.ru>
2018-03-16 12:58:22 -0400

    survey version

--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -19,6 +19,9 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "/../public/survey.html"));
   });
 
+  app.get("/survey2", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/survey2.html"));
+  });
   // If no matching route is found default to home
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "/../public/home.html"));
